0adb5d5c48b45f8fc2f89bd9e6d69a06046deda5
Wanli Zhu <43547021+wanlizhu@users.noreply.github.com>
2026-02-25 18:40:52 -0800

    Update config-profiling-system.sh

--- config-profiling-system.sh
+++ config-profiling-system.sh
@@ -70,16 +70,18 @@ zhu_install() {
     done 
     if (( ${#failed_pkgs[@]} )); then
         for pkg in "${failed_pkgs[@]}"; do 
-            libxcb-errors*) 
-                pushd /tmp >/dev/null 
-                baseurl=http://archive.ubuntu.com/ubuntu/pool/universe/x/xcb-util-errors
-                wget -O libxcb-errors0.deb     $baseurl/libxcb-errors0_1.0.1-4build1_amd64.deb
-                wget -O libxcb-errors-dev.deb  $baseurl/libxcb-errors-dev_1.0.1-4build1_amd64.deb
-                sudo dpkg -i libxcb-errors0.deb libxcb-errors-dev.deb
-                sudo apt -f install -y
-                popd >/dev/null 
-            ;;
-            *) echo "Todo: fallback build of $pkg" ;;
+            case $pkg in 
+                libxcb-errors*) 
+                    pushd /tmp >/dev/null 
+                    baseurl=http://archive.ubuntu.com/ubuntu/pool/universe/x/xcb-util-errors
+                    wget -O libxcb-errors0.deb     $baseurl/libxcb-errors0_1.0.1-4build1_amd64.deb
+                    wget -O libxcb-errors-dev.deb  $baseurl/libxcb-errors-dev_1.0.1-4build1_amd64.deb
+                    sudo dpkg -i libxcb-errors0.deb libxcb-errors-dev.deb
+                    sudo apt -f install -y
+                    popd >/dev/null 
+                ;;
+                *) echo "Todo: fallback build of $pkg" ;;
+            esac
         done 
     fi 
 }
