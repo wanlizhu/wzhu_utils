174a4611fca7ccaf482b759dad4ea7f94749972a
Wanli Zhu <zhu.wanli@icloud.com>
2026-03-06 22:57:43 -0800

    s

--- nvidia-install-driver.sh
+++ nvidia-install-driver.sh
@@ -2,27 +2,6 @@
 
 set -o pipefail 
 
-bfs_find_dir() {
-    local root=$(realpath $1)
-    local name=$2 
-    local -a queue 
-    local queue_head=0
-    queue=($root)
-    while (( queue_head < ${#queue[@]} )); do 
-        root=${queue[$queue_head]}
-        ((queue_head++))
-        if [[ ${root##*/} == $name ]]; then
-            echo $root
-            return 0
-        fi
-        for name in $root/*; do
-            [[ -d $name ]] || continue
-            queue+=($name)
-        done
-    done 
-    return 1
-}
-
 install_local_file() {
     local file=$1 
     [[ $XDG_SESSION_TYPE != tty ]] && return 1
@@ -39,7 +18,7 @@ install_version_build() {
     if [[ -d /mnt/builds/release ]]; then 
         rsync -Pah /mnt/builds/release/display/$(uname -m)$buildtype/$version/NVIDIA-Linux-$(uname -m)-$version.run $HOME || return 1
     else
-        cd $HOME 
+        cd $HOME && 
         wget http://linuxqa.nvidia.com/builds/release/display/$(uname -m)$buildtype/$version/NVIDIA-Linux-$(uname -m)-$version.run || return 1
     fi 
     if [[ -f $HOME/NVIDIA-Linux-$(uname -m)-$version.run ]]; then 
@@ -47,19 +26,9 @@ install_version_build() {
     fi 
 }
 
-install_branch_build() {
-    local branch=$1
-    local buildtype=$([[ -z $2 ]] && echo "" || echo "/$2") 
-    if [[ -d /mnt/builds/daily ]]; then 
-        rsync -Pah /mnt/builds/daily/display/$(uname -m)
-    fi 
-}
-
 if [[ -z $1 || -f $1 ]]; then 
     install_local_file $(realpath $1)
-elif [[ $1 == -version=?* ]]; then
-    value=${1#-version=}
-    version=${value%%:*}
-    install_version_build $version $([[ $value == *:* ]] && echo ${value#*:} || echo)
+elif [[ $1 =~ ^[0-9]+(\.[0-9]+)+$ ]]; then
+    install_version_build "$@"
 fi 
 
