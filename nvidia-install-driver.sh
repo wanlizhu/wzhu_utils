c105446b0c139d09e42c50bd02736ab0d17e5895
Your Name <you@example.com>
2026-03-08 13:25:43 -0700

    s

--- nvidia-install-driver.sh
+++ nvidia-install-driver.sh
@@ -7,15 +7,25 @@ install_local_file() {
     [[ $XDG_SESSION_TYPE != tty ]] && return 1
     [[ -z $file || ! -e $file ]] && return 1
     [[ -z $(which expect) ]] && sudo apt install -y expect 
-    sudo systemctl isolate multi-user
-    sudo systemctl stop nvidia-persistenced 2>/dev/null || sudo nvidia-smi -pm 0 2>/dev/null 
-    sudo rmmod nvidia_drm nvidia_modeset nvidia_uvm nvidia  
-    sleep 3
-    if [[ ! -z $(lsmod | awk '$1 ~ /^nvidia/ {print $1}') ]]; then 
-        sudo modprobe -r $(lsmod | awk '$1 ~ /^nvidia/ {print $1}') 
-    fi 
     sudo chmod +x $file 2>/dev/null 
-    
+
+    # unload nvidia kernel modules 
+    sudo systemctl isolate multi-user
+    if [[ ! -z $(lsmod | grep '^nvidia') ]]; then 
+        sudo systemctl stop nvidia-persistenced 2>/dev/null || sudo nvidia-smi -pm 0 2>/dev/null 
+        sudo rmmod nvidia_drm nvidia_modeset nvidia_uvm nvidia && sleep 3
+        if [[ ! -z $(lsmod | awk '$1 ~ /^nvidia/ {print $1}') ]]; then 
+            sudo modprobe -r $(lsmod | awk '$1 ~ /^nvidia/ {print $1}') 
+        fi 
+    fi 
+
+    # remove apt-based nvidia packaged 
+    nvidia_packages=$(dpkg -l | awk '/^(ii|rc)[[:space:]]+(nvidia|libnvidia|linux-modules-nvidia|xserver-xorg-video-nvidia)/ { print $2 }')
+    if [[ ! -z $nvidia_packages ]]; then
+        sudo apt purge -y $nvidia_packages
+        sudo apt autoremove -y
+    fi
+
 expect - "$file" --ui=none --accept-license --disable-nouveau --no-cc-version-check --install-libglvnd <<'EOF'
     set timeout 600
     spawn sudo {*}$argv
