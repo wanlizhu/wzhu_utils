d78e1988a308cd26f000e90d4e7944fe04537dc2
Wanli Zhu <zhu.wanli@icloud.com>
2026-03-12 17:40:28 -0700

    s

--- nvidia-vpn.sh
+++ nvidia-vpn.sh
@@ -3,10 +3,14 @@
 if [[ -z $(which globalprotect) ]]; then 
     pushd /tmp 
     wget https://d2hvyxt0t758wb.cloudfront.net/gp_install_files/gp_install.sh
-    
+
     chmod +x ./gp_install.sh 
     ./gp_install.sh 
     popd 
 fi 
 
-globalprotect connect --portal nvidia.gpcloudservice.com || echo "Add Nvidia portal in GUI: nvidia.gpcloudservice.com"
+echo "Add Nvidia portal in GUI: nvidia.gpcloudservice.com"
+globalprotect connect --portal nvidia.gpcloudservice.com || {
+    sudo systemctl restart gpd 
+    globalprotect connect --portal nvidia.gpcloudservice.com 
+}
