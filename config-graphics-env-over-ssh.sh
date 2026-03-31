577d0a6d9ee93b4fbb6d04ef8acbda9766bd0375
Wanli Zhu <zhu.wanli@icloud.com>
2026-03-31 11:52:12 -0700

    s

--- config-graphics-env-over-ssh.sh
+++ config-graphics-env-over-ssh.sh
@@ -11,11 +11,13 @@ unset DISPLAY
 unset XAUTHORITY
 unset WAYLAND_DISPLAY
 
-read_env_from_pid() {
-    [[ -r /proc/$1/environ ]] || return 1
-    DISPLAY=$(sudo tr '\0' '\n' </proc/$1/environ | awk -F= '/^DISPLAY=/{print $2; exit}')
-    XAUTHORITY=$(sudo tr '\0' '\n' </proc/$1/environ | awk -F= '/^XAUTHORITY=/{print $2; exit}')
-    WAYLAND_DISPLAY=$(sudo tr '\0' '\n' </proc/$1/environ | awk -F= '/^WAYLAND_DISPLAY=/{print $2; exit}')
+read_env_from_proc() {
+    while read -r pid; do 
+        [[ -z $pid || ! -r /proc/$pid/environ ]] && continue 
+        DISPLAY=$(tr '\0' '\n' </proc/$pid/environ | awk -F= '/^DISPLAY=/{print $2; exit}')
+        XAUTHORITY=$(tr '\0' '\n' </proc/$pid/environ | awk -F= '/^XAUTHORITY=/{print $2; exit}')
+        WAYLAND_DISPLAY=$(tr '\0' '\n' </proc/$pid/environ | awk -F= '/^WAYLAND_DISPLAY=/{print $2; exit}')
+    done < <([[ $1 =~ ^[0-9]+$ ]] && echo $1 || pgrep -u $UID -f $1)
     [[ -n $DISPLAY || -n $XAUTHORITY || -n $WAYLAND_DISPLAY ]]
 }
 
@@ -23,10 +25,11 @@ session_id=$(loginctl list-sessions --no-legend | grep "seat0" | awk '{print $1}
 session_type=$(loginctl show-session $session_id -p Type --value)
 leader_pid=$(loginctl show-session $session_id -p Leader --value 2>/dev/null)
 
-read_env_from_pid $leader_pid ||
-read_env_from_pid "$(pgrep -u $UID -n kwin_wayland)" ||
-read_env_from_pid "$(pgrep -u $UID -n startplasma-wayland)" ||
-read_env_from_pid "$(pgrep -u $UID -n plasmashell)"
+read_env_from_proc $leader_pid ||
+read_env_from_proc kwin_wayland ||
+read_env_from_proc startplasma-wayland ||
+read_env_from_proc plasmashell ||
+read_env_from_proc gnomeshell 
 
 [[ ! -z $DISPLAY ]] && export DISPLAY  
 [[ ! -z $XAUTHORITY ]] && export XAUTHORITY  
