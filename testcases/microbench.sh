64a9fe776a0ad722d3f202c74e68658e98b25714
Wanli Zhu <zhu.wanli@icloud.com>
2026-04-01 23:16:01 -0700

    s

--- testcases/microbench.sh
+++ testcases/microbench.sh
@@ -15,6 +15,18 @@ print_microbench_output_as_csv() {
     fi 
 }
 
+a_over_b() {
+    python3 -c '
+from decimal import Decimal, getcontext
+import sys
+getcontext().prec = 100
+a = Decimal(sys.argv[1])
+b = Decimal(sys.argv[2])
+result = a / b * 100
+print(f"{result:.2f}%")
+' $1 $2 
+}
+
 if [[ -z $1 || ! -e $1 ]]; then 
     echo "Usage: $(basename $0) output1 [output2]"
     exit 1
@@ -25,37 +37,13 @@ if [[ -z "$2" ]]; then
 else 
     print_microbench_output_as_csv "$1" >/tmp/csv1
     print_microbench_output_as_csv "$2" >/tmp/csv2
-    awk -F, '
-        NR == FNR {
-            a[FNR] = $1
-            next
-        }
-        a[FNR] != $1 {
-            printf "Error: row %d mismatch: %s != %s\n", FNR, a[FNR], $1
-            exit 1
-        }
-    ' /tmp/csv1 /tmp/csv2 || exit 1
-    awk -F, '
-        BEGIN {
-            OFS = ","
-        }
-        NR == FNR {
-            csv1_col1[FNR] = $1
-            csv1_col2[FNR] = $2
-            csv1_col3[FNR] = $3
-            next
-        }
-        FNR == 1 {
-            print csv1_col1[FNR], "numeric 1", "numeric 2", "num2 vs num1", csv1_col3[FNR]
-            next
-        }
-        {
-            if (csv1_col2[FNR] + 0 == 0) {
-                ratio = "N/A"
-            } else {
-                ratio = sprintf("%.2f%%", ($2 / csv1_col2[FNR]) * 100)
-            }
-            print csv1_col1[FNR], csv1_col2[FNR], $2, ratio, csv1_col3[FNR]
-        }
-    ' /tmp/csv1 /tmp/csv2 
+    echo "Test case,numeric1,numeric2,numeric2/numeric1,unit"
+    while IFS= read -r line2; do 
+        [[ $line2 == "Test case"* ]] && continue 
+        IFS=, read -r name2 value2 unit2 <<< "$line2"
+        line1=$(cat /tmp/csv1 | grep "$name2")
+        [[ -z $line1 ]] && continue 
+        IFS=, read -r name1 value1 unit1 <<< "$line1"
+        echo "$name2,$value1,$value2,$(a_over_b $value2 $value1),$unit2"
+    done </tmp/csv2 
 fi 
