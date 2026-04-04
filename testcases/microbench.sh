8ac0d087aef7b5835632d9c7c599fcff070baab7
Wanli Zhu <zhu.wanli@icloud.com>
2026-04-04 09:33:39 -0700

    s

--- testcases/microbench.sh
+++ testcases/microbench.sh
@@ -17,38 +17,49 @@ print_microbench_output_as_csv() {
 
 a_over_b() {
     python3 -c '
-from decimal import Decimal, getcontext, InvalidOperation
+from decimal import Decimal, getcontext
 import sys
 try:
     getcontext().prec = 100
     a = Decimal(sys.argv[1])
     b = Decimal(sys.argv[2])
-    result = a / b * 100
-    print(f"{result:.2f}%")
+    result = a / b
+    print(f"{result}")
 except Exception:
     print("N/A")
 ' $1 $2 
 }
 
-if [[ -z $1 || ! -e $1 ]]; then 
-    echo "Usage: $(basename $0) output1 [output2]"
+if [[ $1 == "-h" || $1 == "--help" ]]; then 
+    echo "Usage 1: $(basename $0) -- run for new result"
+    echo "Usage 2: $(basename $0) output1 [output2] -- process existing results"
     exit 1
 fi 
 
-if [[ -z "$2" ]]; then 
-    print_microbench_output_as_csv "$1"
+if [[ -z $1 ]]; then 
+    echo "Saving results to ~/microbench_results[.txt|.csv]"
+    nvperf_vulkan -REST -nullDisplay all | tee ~/microbench_results.txt
+    if [[ -s ~/microbench_results.txt ]]; then 
+        $0 $(realpath ~/microbench_results.txt) | tee ~/microbench_results.csv
+    fi 
+    echo "Generated ~/microbench_results.txt"
+    echo "Generated ~/microbench_results.csv"
 else 
-    print_microbench_output_as_csv "$1" >/tmp/csv1
-    print_microbench_output_as_csv "$2" >/tmp/csv2
-    echo "Test case,numeric1,numeric2,numeric2/numeric1,unit"
-    while IFS= read -r line2; do 
-        [[ $line2 == "Test case"* ]] && continue 
-        IFS=, read -r name2 value2 unit2 <<< "$line2"
-        line1=$(cat /tmp/csv1 | grep "$name2")
-        [[ -z $line1 ]] && continue 
-        IFS=, read -r name1 value1 unit1 <<< "$line1"
-        rate=$(a_over_b $value2 $value1)
-        [[ $rate == "N/A" ]] && continue 
-        echo "$name2,$value1,$value2,$rate,$unit2"
-    done </tmp/csv2 
+    if [[ -z "$2" ]]; then 
+        print_microbench_output_as_csv "$1"
+    else 
+        print_microbench_output_as_csv "$1" >/tmp/csv1
+        print_microbench_output_as_csv "$2" >/tmp/csv2
+        echo "Test case,numeric1,numeric2,numeric2/numeric1,unit"
+        while IFS= read -r line2; do 
+            [[ $line2 == "Test case"* ]] && continue 
+            IFS=, read -r name2 value2 unit2 <<< "$line2"
+            line1=$(cat /tmp/csv1 | grep "$name2")
+            [[ -z $line1 ]] && continue 
+            IFS=, read -r name1 value1 unit1 <<< "$line1"
+            rate=$(a_over_b $value2 $value1)
+            [[ $rate == "N/A" ]] && continue 
+            echo "$name2,$value1,$value2,$rate,$unit2"
+        done </tmp/csv2 
+    fi 
 fi 
